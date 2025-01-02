7d2a41fe4a8685001c4dc918128920d6916a3229
bethmannRolf <devak.robe@gmail.com>
2025-01-02 07:32:28 +0100

    generated structure 2

--- src/app/main-content/our-menu/our-menu.component.ts
+++ src/app/main-content/our-menu/our-menu.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-our-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './our-menu.component.html',
   styleUrl: './our-menu.component.scss'
 })
